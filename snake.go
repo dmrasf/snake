e61f5885a443e6b4e64c1c1a4b0cb6e77b39c747
dmrasf <dmrasf@qq.com>
2020-06-24 11:18:08 +0800

    judge fail

--- snake.go
+++ snake.go
@@ -1,7 +1,7 @@
 package main
 
 type snake struct {
-	//  2,  1,  3,  0
+	//  0; 1; 2; 3
 	Direction int
 	Body      [][2]int
 	IsCanMove bool
@@ -65,4 +65,21 @@ func (sn *snake) eatFood() {
 		nextHead[0][0] += 2
 	}
 	sn.Body = append(nextHead, sn.Body...)
+}
+
+func (sn *snake) isTouchSelf() bool {
+	for _, item := range sn.Body[1:] {
+		if item[0] == sn.Body[0][0] && item[1] == sn.Body[0][1] {
+			return true
+		}
+	}
+	return false
+}
+
+func (sn *snake) isTouchWall(L, R [2]int) bool {
+	headX, headY := sn.Body[0][0], sn.Body[0][1]
+	if headX <= L[0] || headX >= R[0] || headY <= L[1] || headY >= R[1] {
+		return true
+	}
+	return false
 }
